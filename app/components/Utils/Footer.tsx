7b66ded803e7cc7214fec4c89bf858d4aa3343be
25mohit <mohit724agarwal@gmail.com>
2024-10-20 21:58:25 +0530

    Moc Data of Tasks, Updated

--- app/components/Utils/Footer.tsx
+++ app/components/Utils/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='footer'>Footer</footer>
+    <footer className='footer text-blue-300 select-none'>All rights of the website are &copy; Copywrite Reserved 2024-25</footer>
   )
 }
 
